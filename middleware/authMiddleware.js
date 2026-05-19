5ab655d0891bc558ab5f217665b1e5d9cf3493ec
NaveedAhmad-6522 <naveed2k03@gmail.com>
2026-05-19 14:11:21 +0500

    jwt debug logs removed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(req.headers.authorization);
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
       error: "Access denied",
